91c3dbb0489e4abd2d2e48c2cfbc80ac7260035f
Ala Williams <alicja.m.williams@gmail.com>
2025-01-09 09:54:54 +0200

    Lesson 2

---
+++ Week1/lesson2objectProperty.js
@@ -0,0 +1,16 @@
+let car = {
+  brand: 'Toyota',
+  model: 'RAV4',
+  year: 2023
+}
+
+let car2 = {
+  brand: 'Jaguar',
+  model: 'X-Type',
+
+}
+console.log(car)
+console.log(car.year)
+
+car.year = 2022
+console.log(car.year)
