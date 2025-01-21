7fafcd3ae61b5f7bd7fe0b914102e9b5f9a3f362
Ala Williams <alicja.m.williams@gmail.com>
2025-01-21 13:51:01 +0200

    Lesson 7

---
+++ Week3/tsDemo.js
@@ -0,0 +1,4 @@
+var num = 10;
+var num2 = 20;
+var sum = num + num2;
+console.log("The sum of ".concat(num, " and ").concat(num2, " is ").concat(sum));
---
+++ Week3/tsDemo.ts
@@ -0,0 +1,5 @@
+let num : number = 10;
+let num2 : number = 20;
+
+let sum :number = num + num2;
+console.log(`The sum of ${num} and ${num2} is ${sum}`); 
