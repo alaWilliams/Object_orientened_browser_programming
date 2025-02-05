4751b504e5e9189bee3f9d653bdb6724a07c7f4d
Ala Williams <alicja.m.williams@gmail.com>
2025-02-05 19:29:51 +0200

    reading values, calculating contributions, inserting some values to the table

--- bonus-assignment/script.js
+++ bonus-assignment/script.js
@@ -3,24 +3,67 @@ const initialInvestmentInput = document.querySelector('#initialInvestment');
 const monthlyContributionInput = document.querySelector('#monthlyContribution');
 const timeInput = document.querySelector('#time');
 const interestRateInput = document.querySelector('#interestRate');
+const result = document.querySelector('#results');
 
 //reading values
-const initialInvestmentValue = initialInvestmentInput.value;
-const monthlyContributionValue = monthlyContributionInput.value;
-const timeValue = timeInput.value;
-const interestRateValue = interestRateInput.value;
-
-button.addEventListener('click', () => {
-	console.log(initialInvestmentInput.value);
-	console.log(monthlyContributionInput.value);
-	console.log(interestRateInput.value);
-	console.log(timeInput.value);
-});
-
-function calculate(initial, monthly, interest, time) {
-	let rate = interest / 1000;
-	let result = initial * (1 + rate) ** time;
-	return result.toFixed(2);
+function readValue(input) {
+	return Number(input.value)
 }
 
-console.log(calculate(1000, 0, 5, 50));
+
+button.addEventListener('click', () => {
+	const initialInvestmentValue = readValue(initialInvestmentInput)
+	const monthlyContributionValue = readValue(monthlyContributionInput)
+	const timeValue = readValue(timeInput);
+	const interestRateValue = readValue(interestRateInput)
+	const contribution = calculateContributionAfterOneYear(monthlyContributionValue, initialInvestmentValue)
+	createTable(interestRateValue, contribution);
+
+});
+
+function calculateInvestmentValue(initial, monthly, interest, time) {
+	let rate = interest / 100;
+  let contributionsInAYear = monthly * 12
+	let result = initial * (1 + rate) ** time + contributionsInAYear * (((1 + rate) ** time - 1) / rate)
+	return result.toFixed(2)
+}
+
+function calculateContributionAfterOneYear(monthly, initial) {
+	let totalContribution = monthly * 12  + initial
+	return totalContribution
+}
+
+
+function calculateInvestmentValueAfterOneYear(initial, monthly, interest) {
+	let rate = interest / 100
+	let contributionInAYear = monthly * 12;
+	let result = initial * (1 + rate) + contributionInAYear * (1 + rate)
+	return result.toFixed(2)
+}
+
+
+
+
+
+function createTable(interest, contribution) {
+	result.innerHTML = '';
+	const table = document.createElement('table')
+	table.className = 'investmentTable'
+	result.appendChild(table)
+	table.innerHTML = `<tr>
+      <th>Years</th>
+      <th>Investment Value ${interest}%</th>
+      <th>Total contribution</th>
+    </tr>
+    <tr>
+      <td>value1</td>
+      <td>value2</td>
+      <td>${contribution}</td>
+    </tr>
+    <tr>
+      <td>value4</td>
+      <td>value5</td>
+      <td>value6</td>
+    </tr>`
+}
+
